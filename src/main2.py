ab1cbb5bfcfd755016fe6c1b7ec205a4c47f5858
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2021-12-17 09:10:04 +0000

    2

---
+++ src/main2.py
@@ -0,0 +1,20 @@
+a=[]
+print(type(a))
+a=[*range(0,20,1)]
+print(a)
+b=a*2
+z=list.count(b,0)
+print("tipo di z:", type(z))
+print("numero di volte che si presenta 0 in b:",z)
+
+import time
+v1=[*range(10000)]
+v2=[*range(10000)]
+t1=time.perf_counter()
+sum1=v1+v2
+t2=time.perf_counter()
+print("conventional sum:",t2-t1)
+t3=time.perf_counter()
+v1.extend(v2)
+t4=time.perf_counter()
+print("append command:",t4-t3)
